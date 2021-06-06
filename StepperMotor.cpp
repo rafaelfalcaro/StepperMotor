3cee44850cca093b3c21235c97b391b587a8ce7d
Rafael Falcaro <rafael.giarettafalcaro@gmail.com>
2021-06-06 20:25:00 -0300

    Continuação do desenvolvimento

--- StepperMotor.cpp
+++ StepperMotor.cpp
@@ -4,37 +4,89 @@
 //************************************************************************************
 // Funções Públicas
 
+// Mantém o motor na posição atual
+void StepperMotor::Brake() {
+    digitalWrite(pinEnable, LOW);
+    digitalWrite(pinReset , HIGH);
+    digitalWrite(pinSleep , HIGH);
+    digitalWrite(pinStep  , LOW);
+    digitalWrite(pinDir   , LOW);
+}
+
+// Movimento automático até a posição de Home
+void StepperMotor::Home() {
+    //moveServo(HomeOffset);
+}
+
+// Movimento em Jog
+void StepperMotor::Jog(bool _dir) {
+    digitalWrite(pinEnable, LOW);
+    digitalWrite(pinReset , HIGH);
+    digitalWrite(pinSleep , HIGH);
+    digitalWrite(pinStep  , HIGH);
+    digitalWrite(pinDir   , _dir);
+}
+
+// Mantém o motor na posição atual
+void StepperMotor::Kill() {
+    digitalWrite(pinEnable, HIGH);
+    digitalWrite(pinReset , HIGH);
+    digitalWrite(pinSleep , HIGH);
+    digitalWrite(pinStep  , LOW);
+    digitalWrite(pinDir   , LOW);
+}
+
+// Movimento incremental do Servo
+void StepperMotor::MoveInc(int _pos) {
+    //if ((ActPos + _pos >= MinPos) && (ActPos + _pos <= MaxPos)) {
+    //    moveServo(ActPos + _pos + HomeOffset);
+    //}
+}
+
+// Movimento absoluto do Servo
+void StepperMotor::MoveTo(int _pos) {
+    //if ((_pos >= MinPos) && (_pos <= MaxPos)) {
+    //    moveServo(_pos + HomeOffset);
+    //}
+}
+
 // Seleção do modo de funcionamento do Motor
 void StepperMotor::SetMode(byte _mode) {
     if (_mode == 0) {                           // Full Step
         digitalWrite(pinMode0, LOW);
         digitalWrite(pinMode1, LOW);
         digitalWrite(pinMode2, LOW);
+        Stepping = 1;
     }
     else if (_mode == 1) {                      // Half Step
         digitalWrite(pinMode0, HIGH);
         digitalWrite(pinMode1, LOW);
         digitalWrite(pinMode2, LOW);
+        Stepping = 1/2;
     }
     else if (_mode == 2) {                      // 1/4 Microstepping
         digitalWrite(pinMode0, LOW);
         digitalWrite(pinMode1, HIGH);
         digitalWrite(pinMode2, LOW);
+        Stepping = 1/4;
     }
     else if (_mode == 3) {                      // 1/8 Microstepping
         digitalWrite(pinMode0, HIGH);
         digitalWrite(pinMode1, HIGH);
         digitalWrite(pinMode2, LOW);
+        Stepping = 1/8;
     }
     else if (_mode == 4) {                      // 1/16 Microstepping
         digitalWrite(pinMode0, LOW);
         digitalWrite(pinMode1, LOW);
         digitalWrite(pinMode2, HIGH);
+        Stepping = 1/16;
     }
     else if (_mode == 5) {                      // 1/32 Microstepping
         digitalWrite(pinMode0, HIGH);
         digitalWrite(pinMode1, HIGH);
         digitalWrite(pinMode2, HIGH);
+        Stepping = 1/32;
     }
 }
 
@@ -59,56 +111,58 @@ void StepperMotor::SetPins(byte _pFault, byte _pDir, byte _pSleep, byte _pReset,
     pinMode(pinEnable, OUTPUT);
     pinMode(pinMode0 , OUTPUT);
     pinMode(pinStep  , OUTPUT);
-    X9C103S(_pINC, _pUD, _pCS);
+
+    DigiPot.SetPins(_pCS, _pINC, _pUD);
 }
 
 
 //************************************************************************************
 // Funções Privadas
 
-/*void StepperMotor::Jog(byte _dir) {
-    digitalWrite(PinEnable, LOW);
-    digitalWrite(PinReset, HIGH);
-    digitalWrite(PinSleep, HIGH);
-    digitalWrite(PinStep, HIGH);
-    digitalWrite(PinDir, _dir);
+// Move (absoluto) levando em consideração velocidade e aceleração
+void StepperMotor::moveMotor(int _pos) {
+    
+    setSpeed();
+    
+    /*
+    #include "PID.h"
+
+    PID::PID() {
+    }
+
+    double PID::compute(double PV) {
+
+        error = Setpoint - PV;
+
+        float deltaTime = (millis() - lastCompute) / 1000.0;
+        lastCompute = millis();
+
+        if (abs(error) > DeadBand) {
+            P = error * Kp;
+            I = I + (error * Ki) * deltaTime;
+            D = (lastSample - PV) * Kd / deltaTime;
+        }
+
+        lastSample = PV;
+
+        return P + I + D + 50;
+        //return map((P + I + D), (Setpoint - 100), Setpoint, 0, 100);
+    }
+    */
+
+    /*double waitTime = (-0.1919 * JogSpeed) + 20.192;
+    while (_pos != Pos) {
+        if (_pos > Pos) Pos++;
+        else Pos -= 1;
+        ActPos = Pos - HomeOffset;
+        servo.write(Pos);
+        delay(waitTime);
+    }*/
 }
 
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-/*
-#include "PID.h"
-
-PID::PID() {
+// Altera a velocidade do motor
+void StepperMotor::setSpeed() {
+    double Velo = 1.8*JogSpeed;     // 360° / (200 PPR / Stepping) * JogSpeed
+    uint8_t Counts = 0.0099009900990099*R5+99.009900990099-0.00852716850632432*R5*RS*Stepping*VIN/(CT*RL*RT*Velo);
+    DigiPot.Set(Counts);            // Escrita dos Counts no módulo X9C103S
 }
-
-double PID::compute(double PV) {
-
-	error = Setpoint - PV;
-
-	float deltaTime = (millis() - lastCompute) / 1000.0;
-	lastCompute = millis();
-
-	if (abs(error) > DeadBand) {
-		P = error * Kp;
-		I = I + (error * Ki) * deltaTime;
-		D = (lastSample - PV) * Kd / deltaTime;
-	}
-
-	lastSample = PV;
-
-	return P + I + D + 50;
-	//return map((P + I + D), (Setpoint - 100), Setpoint, 0, 100);
-}
-*/
--- StepperMotor.h
+++ StepperMotor.h
@@ -2,20 +2,55 @@
 #define StepperMotor_h
 
 #include <Arduino.h>
-#include <X9C103S.h>        // Biblioteca de controle do potenciômetro digital X9C103S
+#include <X9C103S.h>            // Biblioteca de controle do potenciômetro digital X9C103S
+
+// Definições das constantes
+#define RS       8400.0         // RV1
+#define RL       100047.0       // R4 + R6
+#define RT       5600.0         // R2
+#define CT       0.00000001     // C4
+#define VIN      5.0            // [V]
+#define R5       2700.0         // R5
 
 // Classe Principal
 class StepperMotor {
     public:
         // Funções
+        void Brake();                       // Mantém o motor na posição atual
+        void Home();                        // Movimento automático até a posição de Home
+        void Jog(bool _dir);                // Movimento em Jog
+        void Kill();                        // Libera o eixo do Motor
+        void MoveInc(int _pos);             // Movimento incremental
+        void MoveTo(int _pos);              // Movimento absoluto
         void SetMode(byte Mode);            // Seleção do modo de funcionamento do motor
         void SetPins(byte _pFault, byte _pDir, byte _pSleep, byte _pReset, byte _pMode2, byte _pMode1, byte _pEnable, byte _pMode0, byte _pStep, byte _pCS, byte _pINC, byte _pUD);   // Define automaticamente os pinos do Driver como IN/OUT
-        
+
+        // Objeto da biblioteca X9C103S
+        X9C103S DigiPot;
+
         // Parametrização do Motor
-        float Vel;                          // Velocidade de movimento [°/s]
         byte Dir;                           // Sentido de Giro: 0->CW / 1->CCW
+        byte HomeOffset;                    // Offset de posição [cts]
+        byte JogSpeed;                      // Setpoint de velocidade de movimento [cts/s]
+        byte JogTa;                         // Tempo de aceleração [s]
+        byte JogTd;                         // Tempo de desaceleração [s]
+        int MaxPos;                         // Posição Máxima permitida [cts]
+        int MinPos;                         // Posição Mínima permitida [cts]
+        double Mres;                        // Resolução do movimento [°]
+        
+        // Status do Motor
+        int ActPos;                         // Posição atual processada do Servo [cts]
+        int ActPos2;                        // Posição atual processada do Servo [°]
+        byte Pos;                           // Posição atual não processada do Servo [cts]
+        byte Pos2;                          // Posição atual não processada do Servo [°]
+        byte Velocity;                      // Velocidade atual de movimento do Servo [cts/s]
+        byte Velocity2;                     // Velocidade atual de movimento do Servo [°/s]
 
     private:
+        // Funções
+        void moveMotor(int _pos);           // Move (absoluto) levando em consideração velocidade e aceleração
+        void setSpeed();                    // Altera a velocidade do motor
+
         // Pinos do Driver DRV8825
         byte pinFault;                      // Diagnóstico de Falha
         byte pinDir;                        // Sentido de Giro
@@ -26,6 +61,9 @@ class StepperMotor {
         byte pinEnable;                     // Enable Driver
         byte pinMode0;                      // Seleção de modo de operação 0
         byte pinStep;                       // Steps para o Motor
+
+        // Variáveis de lógica
+        double Stepping;                    // Razão de passos completos do motor
 };
 
 #endif
